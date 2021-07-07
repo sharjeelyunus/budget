ea3cbce9811cec485a90d2cb7cfdb60e6b1711ff
sharjeelyunus <sharjeel924@gmail.com>
2021-07-08 04:02:09 +0500

    👌 IMPROVE: Reset Data & Logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,60 @@ import logo from '../assets/logo.png';
 import { Avatar } from '@material-ui/core';
 import './Header.css';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from '../firebase';
+import { auth, db } from '../firebase';
+import Button from '@material-ui/core/Button';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+    paper: {
+        marginRight: theme.spacing(2),
+    },
+}));
 
 const Header = () => {
     const [user] = useAuthState(auth);
+
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+    const anchorRef = React.useRef(null);
+
+    const handleToggle = () => {
+        setOpen((prevOpen) => !prevOpen);
+    };
+
+    const handleClose = (event) => {
+        if (anchorRef.current && anchorRef.current.contains(event.target)) {
+            return;
+        }
+
+        setOpen(false);
+    };
+
+    function handleListKeyDown(event) {
+        if (event.key === 'Tab') {
+            event.preventDefault();
+            setOpen(false);
+        }
+    }
+
+    // return focus to the button when we transitioned from !open -> open
+    const prevOpen = React.useRef(open);
+    React.useEffect(() => {
+        if (prevOpen.current === true && open === false) {
+            anchorRef.current.focus();
+        }
+
+        prevOpen.current = open;
+    }, [open]);
 
     const monthNames = ["January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"
@@ -15,6 +65,21 @@ const Header = () => {
     let date = newDate.getDate();
     const monthName = monthNames[newDate.getMonth()];
 
+    const ResetAllData = () => {
+        const TransToBeDeleted = db.collection(`${user.email}`).doc('Transactions').collection('Transaction');
+        TransToBeDeleted.get().then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                doc.ref.delete();
+            });
+        });
+
+        const LoansToBeDeleted = db.collection(`${user.email}`).doc('Loans').collection('Loan');
+        LoansToBeDeleted.get().then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                doc.ref.delete();
+            });
+        });
+    }
 
     return (
         <div className="header">
@@ -27,12 +92,40 @@ const Header = () => {
             </div>
             <div className="header__user">
                 <h2><span>Hello,</span> {user?.displayName}!</h2>
-                <Avatar
-                    onClick={() => auth.signOut()}
-                    alt={user?.displayName}
-                    src={user?.photoURL}
-                    title="SignOut"
-                />
+                <div className={classes.root}>
+                    <div>
+                        <Button
+                            ref={anchorRef}
+                            aria-controls={open ? 'menu-list-grow' : undefined}
+                            aria-haspopup="true"
+                            onClick={handleToggle}
+                        >
+                            <Avatar
+
+                                alt={user?.displayName}
+                                src={user?.photoURL}
+                                title="SignOut"
+                            />
+                        </Button>
+                        <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+                            {({ TransitionProps, placement }) => (
+                                <Grow
+                                    {...TransitionProps}
+                                    style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+                                >
+                                    <Paper>
+                                        <ClickAwayListener onClickAway={handleClose}>
+                                            <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+                                                <MenuItem onClick={ResetAllData}>Reset All Data</MenuItem>
+                                                <MenuItem onClick={() => auth.signOut()}>Logout</MenuItem>
+                                            </MenuList>
+                                        </ClickAwayListener>
+                                    </Paper>
+                                </Grow>
+                            )}
+                        </Popper>
+                    </div>
+                </div>
             </div>
         </div>
     )
