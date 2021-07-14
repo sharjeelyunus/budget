824df290bfe3518d1420553fe1ece06e4fcf0578
sharjeelyunus <sharjeel924@gmail.com>
2021-07-15 03:13:24 +0500

    📦 NEW: Loading State

--- src/App.js
+++ src/App.js
@@ -6,9 +6,27 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from './firebase';
 import Login from './components/Login';
+import logo from './assets/logo.png';
+import Spinner from 'react-spinkit';
 
 function App() {
-  const [user] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
+
+  if (loading) {
+    return (
+      <div className="app__loading">
+        <div className="loading__content">
+          <img src={logo} alt="" />
+          <Spinner
+            name="ball-spin-fade-loader"
+            color="purple"
+            fadein="none"
+          />
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className="App">
       <Router>
