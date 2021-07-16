988bd50bc1d882c8dd2589adabd458ceefa6afea
sharjeelyunus <sharjeel924@gmail.com>
2021-07-16 10:12:25 +0500

    👌 IMPROVE: Alert on deleting all data

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,6 +12,30 @@ import Popper from '@material-ui/core/Popper';
 import MenuItem from '@material-ui/core/MenuItem';
 import MenuList from '@material-ui/core/MenuList';
 import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+
+const useModalStyles = makeStyles((theme) => ({
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+        cursor: 'pointer',
+        display: 'flex',
+        alignItems: 'center',
+        color: 'red',
+        fontWeight: 700,
+        fontSize: '20px',
+    },
+}));
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -26,8 +50,18 @@ const Header = () => {
     const [user] = useAuthState(auth);
 
     const classes = useStyles();
+    const modalClasses = useModalStyles();
+    const [openModal, setOpenModal] = React.useState(false);
     const [open, setOpen] = React.useState(false);
     const anchorRef = React.useRef(null);
+
+    const handleOpenModal = () => {
+        setOpenModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setOpenModal(false);
+    };
 
     const handleToggle = () => {
         setOpen((prevOpen) => !prevOpen);
@@ -90,6 +124,24 @@ const Header = () => {
 
     return (
         <div className="header">
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                className={modalClasses.modal}
+                open={openModal}
+                onClose={handleCloseModal}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={openModal}>
+                    <div className={modalClasses.paper} onClick={() => { ResetAllData(); handleCloseModal() }}>
+                        Reset All Data <DeleteForeverIcon />
+                    </div>
+                </Fade>
+            </Modal>
             <div className="header__logo">
                 <img src={logo} alt="" />
                 <h1>Budget</h1>
@@ -121,7 +173,7 @@ const Header = () => {
                                     <Paper>
                                         <ClickAwayListener onClickAway={handleClose}>
                                             <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
-                                                <MenuItem onClick={ResetAllData}>Reset All Data</MenuItem>
+                                                <MenuItem onClick={handleOpenModal}>Reset All Data</MenuItem>
                                                 <MenuItem onClick={() => auth.signOut()}>Logout</MenuItem>
                                             </MenuList>
                                         </ClickAwayListener>
